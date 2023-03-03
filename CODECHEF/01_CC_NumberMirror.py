70b519a9768a2d93d79690d8269eaeae352b4b9f
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-03 15:28:03 +0530

    Create 01_CC_NumberMirror.py
    START01

---
+++ CODECHEF/01_CC_NumberMirror.py
@@ -0,0 +1,12 @@
+print(int(input()))
+
+"""
+Problem:
+Write a program that takes a number N as the input, and prints it to the output.
+
+Input Format
+The only line contains a single integer.
+
+Output Format:
+Output the answer in a single line.
+"""
