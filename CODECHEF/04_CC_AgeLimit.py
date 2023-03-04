6b550850deb579a42e770903e4bfb26fa04b3814
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-04 18:55:01 +0530

    AGELIMIT

---
+++ CODECHEF/04_CC_AgeLimit.py
@@ -0,0 +1,12 @@
+t=int(input())
+for i in range(t):
+    x,y,a=map(int,input().split())
+    if a>=x and a<y:
+        print("YES")
+    else:
+        print("NO")
+
+"""
+Problem Link :
+https://www.codechef.com/problems/AGELIMIT
+"""
