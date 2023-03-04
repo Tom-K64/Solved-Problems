bc6a5b99dc6e19a6bb57f06a6fd08a9fe0a8ceae
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-04 18:59:13 +0530

    Q9

---
+++ LEETCODE/02_LC_PalindromeNumber.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+        else:
+            temp=x
+            newnum=0
+            while temp>0:
+                rem=temp%10
+                temp=int(temp/10)
+                newnum= (newnum*10)+rem
+            if newnum==x:
+                return True
+            else:
+                return False
+"""
+Problem Link :
+https://leetcode.com/problems/palindrome-number/
+"""
