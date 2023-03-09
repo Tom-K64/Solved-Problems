b3e0e7f5786ae0b0ffb08b904fc2608d85b84574
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-09 08:54:29 +0530

    WATERCONS

---
+++ CODECHEF/11_CC_WATERCONS.py
@@ -0,0 +1,11 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/WATERCONS
+"""
+t=int(input())
+for i in range(t):
+    x=int(input())
+    if x<2000:
+        print("NO")
+    else:
+        print("YES")
