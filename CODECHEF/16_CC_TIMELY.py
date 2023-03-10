9c613227c6613bf7208767e494243a03511dfdc4
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-10 13:55:18 +0530

    TIMELY

---
+++ CODECHEF/16_CC_TIMELY.py
@@ -0,0 +1,11 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/TIMELY
+"""
+t=int(input())
+for i in range(t):
+    x=int(input())
+    if x>=30:
+        print("YES")
+    else:
+        print("NO")
