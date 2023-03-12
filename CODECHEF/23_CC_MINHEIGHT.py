80f9027d907f75191eb23265852c5a78701228fd
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-12 09:13:27 +0530

    MINHEIGHT

---
+++ CODECHEF/23_CC_MINHEIGHT.py
@@ -0,0 +1,11 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/MINHEIGHT
+"""
+t=int(input())
+for i in range(t):
+    x,h=map(int,input().split())
+    if x>=h:
+        print("YES")
+    else:
+        print("NO")
