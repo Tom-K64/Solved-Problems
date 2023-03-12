a29ed9005cf5df5cf2c9fc4d9820d7227f76c6d8
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-12 09:17:23 +0530

    PAR2

---
+++ CODECHEF/25_CC_PAR2.py
@@ -0,0 +1,11 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/PAR2
+"""
+t=int(input())
+for i in range(t):
+    x=int(input())
+    if x%2==0:
+        print("YES")
+    else:
+        print("NO")
