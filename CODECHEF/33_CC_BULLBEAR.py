3ed8e43cb755f6b97ee075d45679cce5f35c2558
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-14 10:00:21 +0530

    BULLBEAR.

---
+++ CODECHEF/33_CC_BULLBEAR.py
@@ -0,0 +1,13 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/BULLBEAR
+"""
+t=int(input())
+for i in range(t):
+    x,y=map(int,input().split())
+    if y>x:
+        print("PROFIT")
+    elif y<x:
+        print("LOSS")
+    else:
+        print("NEUTRAL")
