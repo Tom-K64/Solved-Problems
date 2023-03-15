eddb9541c856ede4505941cc56db7494f0974c9e
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-15 16:00:01 +0530

    ONEMORE

---
+++ CODECHEF/40_CC_ONEMORE.py
@@ -0,0 +1,11 @@
+"""
+Problem Link :
+https://www.codechef.com/START80D/problems/ONEMORE
+"""
+t=int(input())
+for i in range(t):
+    x=int(input())
+    if x<= 24:
+        print("NO")
+    else:
+        print("YES")
