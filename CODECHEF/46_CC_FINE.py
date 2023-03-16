0e98510e2ae22639fe6f0197d41426766873a2af
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-16 21:21:43 +0530

    FINE

---
+++ CODECHEF/46_CC_FINE.py
@@ -0,0 +1,13 @@
+"""
+Problem Link:
+https://www.codechef.com/problems/FINE
+"""
+t=int(input())
+for i in range(t):
+    x=int(input())
+    if x<=70:
+        print(0)
+    elif x>70 and x<=100:
+        print(500)
+    else:
+        print(2000)
