04e39a51fc0a16e2c0dfec3284b4ecf929993d90
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-18 11:30:44 +0530

    SLEEP

---
+++ CODECHEF/53_CC_SLEEP.py
@@ -0,0 +1,11 @@
+"""
+Problem Link:
+https://www.codechef.com/problems/SLEEP
+"""
+t=int(input())
+for i in range(t):
+    x=int(input())
+    if x<7:
+        print("YES")
+    else:
+        print("NO")
