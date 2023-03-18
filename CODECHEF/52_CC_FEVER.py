b9fdc36761c16221e2a0e4826b58dfd783f44c94
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-18 11:30:24 +0530

    FEVER

---
+++ CODECHEF/52_CC_FEVER.py
@@ -0,0 +1,11 @@
+"""
+Problem Link:
+https://www.codechef.com/problems/FEVER
+"""
+t=int(input())
+for i in range(t):
+    f=int(input())
+    if f>98:
+        print("YES")
+    else:
+        print("NO")
