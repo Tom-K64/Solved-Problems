122119de7e2f6ee9021853ed272e6e913dd4bc7c
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-18 11:30:05 +0530

    READPAGES

---
+++ CODECHEF/51_CC_READPAGES.py
@@ -0,0 +1,11 @@
+"""
+Problem Link:
+https://www.codechef.com/problems/READPAGES
+"""
+t=int(input())
+for i in range(t):
+    n,x,y=map(int,input().split())
+    if n<=(x*y):
+        print("YES")
+    else:
+        print("NO")
