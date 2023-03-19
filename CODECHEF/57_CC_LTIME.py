1d58e90ca5bf96a86f3b24ea5c7ba02e65c92f67
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-19 11:10:39 +0530

    LTIME

---
+++ CODECHEF/57_CC_LTIME.py
@@ -0,0 +1,11 @@
+"""
+Problem Link:
+https://www.codechef.com/problems/LTIME
+"""
+t=int(input())
+for i in range(t):
+    x=int(input())
+    if x>=1 and x<=4:
+        print("YES")
+    else:
+        print("NO")
