c0e145af0e56702c48a9c6e2bac6f988138900a2
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-21 22:31:31 +0530

    CARTRIP

---
+++ CODECHEF/66_CC_CARTRIP.py
@@ -0,0 +1,11 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/CARTRIP
+"""
+t=int(input())
+for i in range(t):
+    x=int(input())
+    if x<=300:
+        print(3000)
+    else:
+        print(x*10)
