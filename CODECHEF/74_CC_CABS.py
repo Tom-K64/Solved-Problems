3f821eb2fd6b17fea6ac495a4bf3d9763c57bc37
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-22 12:50:17 +0530

    CABS

---
+++ CODECHEF/74_CC_CABS.py
@@ -0,0 +1,13 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/CABS
+"""
+t=int(input())
+for i in range(t):
+    x,y=map(int,input().split())
+    if x<y:
+        print("FIRST")
+    elif y<x:
+        print("SECOND")
+    else:
+        print("ANY")
