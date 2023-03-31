51035c2e5c667ac17fc8bd11681415f1e74f993c
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-03-31 15:49:28 +0530

    MANIPULATE

---
+++ CODECHEF/87_CC_MANIPULATE.py
@@ -0,0 +1,8 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/MANIPULATE
+"""
+t=int(input())
+for i in range(t):
+    x,y=map(int,input().split())
+    print("YES") if x>=y else print("NO")
