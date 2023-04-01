f159df5cca20680fb20a38b614be56bcf9c844a3
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-04-01 11:44:18 +0530

    CMASKS

---
+++ CODECHEF/91_CC_CMASKS.py
@@ -0,0 +1,11 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/CMASKS
+"""
+t=int(input())
+for i in range(t):
+    x,y=map(int,input().split())
+    if (x*100)<(y*10):
+        print("Disposable")
+    else:
+        print("Cloth")
