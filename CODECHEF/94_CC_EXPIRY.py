2402bdeb0963a11ea5c7c3996e82e78c775ef3fc
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-04-01 11:45:42 +0530

    EXPIRY

---
+++ CODECHEF/94_CC_EXPIRY.py
@@ -0,0 +1,9 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/EXPIRY
+"""
+t=int(input())
+for i in range(t):
+    n,m,k=map(int,input().split())
+    print("YES") if (m*k)>=n else print("NO")
+    
