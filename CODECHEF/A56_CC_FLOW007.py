9f9bc3a72df17f2c0945348f2e88f51fd3444d58
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-04-16 21:40:49 +0530

    FLOW007

---
+++ CODECHEF/A56_CC_FLOW007.py
@@ -0,0 +1,17 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/FLOW007
+"""
+t = int(input())
+for i in range(t):
+    reverse = 0
+    num = int(input())
+    while(num):
+        remainder = num%10
+        reverse =reverse*10+remainder
+        num = int(num/10)
+    print(reverse)
+
+
+
+
