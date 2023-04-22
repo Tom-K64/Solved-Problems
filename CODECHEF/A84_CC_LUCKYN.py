8a1ec3708e153480a3e708e986a732adf622fff0
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-04-22 11:23:17 +0530

    LUCKYN

---
+++ CODECHEF/A84_CC_LUCKYN.py
@@ -0,0 +1,11 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/LUCKYN
+"""
+t=int(input())
+for i in range(t):
+    x=int(input())
+    if "7" in str(x):
+        print("YES")
+    else:
+        print("NO")
