122aff84e086f903df8d4128cfc13a11ea356fc7
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-05-01 00:37:24 +0530

    FLOW008

---
+++ CODECHEF/B33_CC_FLOW008.py
@@ -0,0 +1,11 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/FLOW008
+"""
+t=int(input())
+for i in range(t):
+    n=int(input())
+    if n<10:
+        print("Thanks for helping Chef!")
+    else:
+        print(-1)
