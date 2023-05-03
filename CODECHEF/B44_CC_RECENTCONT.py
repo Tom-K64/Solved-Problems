bc6e4662ace93b9ca5f8500f819aef005a69f76d
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-05-03 16:42:59 +0530

    RECENTCONT

---
+++ CODECHEF/B44_CC_RECENTCONT.py
@@ -0,0 +1,9 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/RECENTCONT
+"""
+t=int(input())
+for i in range(t):
+    n=int(input())
+    concode=list(input().split())
+    print(concode.count("START38"),concode.count("LTIME108"))
