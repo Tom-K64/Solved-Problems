16e87a2b3da424da80f73bfcae69e0ec9e225d3e
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-05-05 12:51:08 +0530

    TYRES

---
+++ CODECHEF/B54_CC_TYRES.py
@@ -0,0 +1,11 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/TYRES
+"""
+t=int(input())
+for i in range(t):
+    n=int(input())
+    if n%4==0:
+        print("NO")
+    else:
+        print("YES")
