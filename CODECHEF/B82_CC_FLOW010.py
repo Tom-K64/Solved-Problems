8b639674ccb37a30edcd27097f1aa03f1e80e107
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-05-11 00:05:50 +0530

    FLOW010

---
+++ CODECHEF/B82_CC_FLOW010.py
@@ -0,0 +1,17 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/FLOW010
+"""
+t=int(input())
+for i in range(t):
+    x=input()
+    if x=="B" or x=="b":
+        print("BattleShip")
+    elif x=="C" or x=="c":
+        print("Cruiser")
+    elif x=="D" or x=="d":
+        print("Destroyer")
+    elif x=="F" or x=="f":
+        print("Frigate")
+    else:
+        print("Wrong")
