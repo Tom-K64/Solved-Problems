c8c5020d8579bd8754415826661246356d15031f
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-05-13 16:46:40 +0530

    VISA

---
+++ CODECHEF/B92_CC_VISA.py
@@ -0,0 +1,11 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/VISA
+"""
+t=int(input())
+for i in range(t):
+    x1,x2,y1,y2,z1,z2=map(int,input().split())
+    if x2>=x1 and y2>=y1 and z2<=z1:
+        print("YES")
+    else:
+        print("NO")
