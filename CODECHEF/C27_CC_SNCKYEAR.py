29cd174ad226f5f08a392cfc8c22992dd05a891d
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-05-28 20:08:07 +0530

    SNCKYEAR

---
+++ CODECHEF/C27_CC_SNCKYEAR.py
@@ -0,0 +1,12 @@
+"""
+Problem Link :
+https://www.codechef.com/problems/SNCKYEAR
+"""
+t=int(input())
+year=[2010,2015,2016,2017,2019]
+for i in range(t):
+    n=int(input())
+    if n in year:
+        print("HOSTED")
+    else:
+        print("NOT HOSTED")
