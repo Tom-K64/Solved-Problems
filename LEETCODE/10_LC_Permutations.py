7f59b58112cfa3c88ee6e4caeada871b658eac64
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-09-18 20:44:57 +0530

    Q-46

---
+++ LEETCODE/10_LC_Permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums):
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                ans.append(curr[:])
+                return
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    backtrack(curr)
+                    curr.pop()
+        ans = []
+        backtrack([])
+        return ans
+"""
+Problem Link:
+https://leetcode.com/problems/permutations/
+"""
