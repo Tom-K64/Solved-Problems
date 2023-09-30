d61b6451f27644ab6420da84b4da4eca16eff449
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-09-30 13:50:03 +0530

    Q-74

---
+++ LEETCODE/19_LC_2DMatrix.py
@@ -0,0 +1,21 @@
+class Solution:
+  def searchMatrix(self, matrix, target):
+    if not matrix:
+      return False
+    m,n,left = len(matrix),len(matrix[0]),0
+    right = m * n
+    while left < right:
+      mid = (left + right) // 2
+      i = mid // n
+      j = mid % n
+      if matrix[i][j] == target:
+        return True
+      if matrix[i][j] < target:
+        left = mid + 1
+      else:
+        right = mid
+    return False
+"""
+Problem Link :
+https://leetcode.com/problems/simplify-path/
+"""
