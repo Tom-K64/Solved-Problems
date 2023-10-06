d7a280beeee427232a105ed86a5224c2d0ebd280
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-10-06 11:33:43 +0530

    Q-92

---
+++ LEETCODE/24_LC_ReverseLinkedList2.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+  def reverseBetween(self, head, left, right):
+    """
+    :type head: ListNode
+    :type left: int
+    :type right: int
+    :rtype: ListNode
+    """
+    if left == 1:
+      return self.reverseN(head, right)
+
+    head.next = self.reverseBetween(head.next, left - 1, right - 1)
+    return head
+
+  def reverseN(self, head, n):
+    if n == 1:
+      return head
+
+    newHead = self.reverseN(head.next, n - 1)
+    headNext = head.next
+    head.next = headNext.next
+    headNext.next = head
+    return newHead
+
+"""
+Problem Link :
+https://leetcode.com/problems/reverse-linked-list-ii/
+"""
