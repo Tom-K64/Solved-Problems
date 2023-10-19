fa622e051cd4ac3ab50dfcd9b2773ec1030f19f4
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-10-19 20:41:47 +0530

    Q-178

---
+++ LEETCODE/44_LC_RankScore.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+    scores['rank'] = scores['score'].rank(method='dense', ascending=False)
+    result_df = scores.drop('id',axis=1).sort_values(by='score', ascending=False)
+    
+    return result_df
+
+"""
+Problem Link:
+https://leetcode.com/problems/rank-scores/
+"""
