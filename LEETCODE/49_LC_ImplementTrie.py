599d5b19be8bf05ffab50e885f852b77b2fb7afa
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-10-22 10:46:50 +0530

    Q-208

---
+++ LEETCODE/49_LC_ImplementTrie.py
@@ -0,0 +1,36 @@
+class TrieNode:
+  def __init__(self):
+    self.children: Dict[str, TrieNode] = collections.defaultdict(TrieNode)
+    self.isWord = False
+
+
+class Trie:
+  def __init__(self):
+    self.root = TrieNode()
+
+  def insert(self, word):
+    node: TrieNode = self.root
+    for c in word:
+      if c not in node.children:
+        node.children[c] = TrieNode()
+      node = node.children[c]
+    node.isWord = True
+
+  def search(self, word):
+    node: TrieNode = self._find(word)
+    return node and node.isWord
+
+  def startsWith(self, prefix):
+    return self._find(prefix)
+
+  def _find(self, prefix):
+    node: TrieNode = self.root
+    for c in prefix:
+      if c not in node.children:
+        return None
+      node = node.children[c]
+    return node
+"""
+Problem Link:
+https://leetcode.com/problems/implement-trie-prefix-tree/
+"""
