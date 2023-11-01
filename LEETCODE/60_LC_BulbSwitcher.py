aa78e5aca452de58626b52648c4dc8e87981e55d
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-11-01 21:34:09 +0530

    Q-319

---
+++ LEETCODE/60_LC_BulbSwitcher.py
@@ -0,0 +1,12 @@
+"""
+Problem Link:
+https://leetcode.com/problems/bulb-switcher/
+"""
+
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(sqrt(n))
