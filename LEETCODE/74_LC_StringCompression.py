4d36fb06ce8306aee56f4317d5db952395a0f395
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-11-20 23:18:16 +0530

    Q-443

---
+++ LEETCODE/74_LC_StringCompression.py
@@ -0,0 +1,27 @@
+"""
+Problem Link:
+https://leetcode.com/problems/string-compression/
+"""
+
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        i = 0
+        res = 0
+        while i < len(chars):
+            group_length = 1
+            while (i + group_length < len(chars)
+                   and chars[i + group_length] == chars[i]):
+                group_length += 1
+            chars[res] = chars[i]
+            res += 1
+            if group_length > 1:
+                str_repr = str(group_length)
+                chars[res:res+len(str_repr)] = list(str_repr)
+                res += len(str_repr)
+            i += group_length
+        return res
+        
