43d9c4dacb5c212b5911cb1c2472fe42fadf2a51
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-12-05 23:49:06 +0530

    Q-515

---
+++ LEETCODE/82_LC_FindLargestValue.py
@@ -0,0 +1,27 @@
+"""
+Problem Link:
+https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+  def largestValues(self, root):
+    if not root:
+      return []
+    ans = []
+    q = collections.deque([root])
+    while q:
+      maxi = -math.inf
+      for _ in range(len(q)):
+        root = q.popleft()
+        maxi = max(maxi, root.val)
+        if root.left:
+          q.append(root.left)
+        if root.right:
+          q.append(root.right)
+      ans.append(maxi)
+    return ans
