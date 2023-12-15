0abd61375a8254584c1d94c59a55a5b54b2736ec
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2023-12-16 01:06:01 +0530

    Q-586

---
+++ LEETCODE/91_LC_CustomerPlacingtheLargest.py
@@ -0,0 +1,12 @@
+"""
+Problem Link:
+https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/
+"""
+
+import pandas as pd
+
+def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
+    customer_order_counts = orders.groupby('customer_number')['order_number'].count().reset_index()
+    max_orders_customer = customer_order_counts[customer_order_counts['order_number'] == customer_order_counts['order_number'].max()][['customer_number']]
+    
+    return max_orders_customer
