025e18f0c8e9b807cc6a39f88cfa55921a776c41
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-01-03 21:17:04 +0530

    Q-704

---
+++ LEETCODE/105_LC_BinarySearch.py
@@ -0,0 +1,16 @@
+"""
+Problem Link:
+https://leetcode.com/problems/binary-search/
+"""
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target in nums:
+            return nums.index(target)
+        else:
+            return -1
