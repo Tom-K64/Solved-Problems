1e9ab7e092942e35c2e9322e5226f6583e438b0c
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-01-27 12:30:08 +0530

    Q-880

---
+++ LEETCODE/133_LC_DecodedStr.py
@@ -0,0 +1,21 @@
+"""
+Problem Link:
+https://leetcode.com/problems/decoded-string-at-index/description/
+"""
+
+class Solution:
+  def decodeAtIndex(self, s: str, k: int) -> str:
+    size = 0
+    for c in s:
+      if c.isdigit():
+        size *= int(c)
+      else:
+        size += 1
+    for c in reversed(s):
+      k %= size
+      if k == 0 and c.isalpha():
+        return c
+      if c.isdigit():
+        size //= int(c)
+      else:
+        size -= 1
