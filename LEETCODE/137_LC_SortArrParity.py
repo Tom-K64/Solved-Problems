f44302a5ca1ca7a273c606cf740f57689852d390
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-01-29 16:20:26 +0530

    Q-905

---
+++ LEETCODE/137_LC_SortArrParity.py
@@ -0,0 +1,17 @@
+"""
+Problem Link:
+https://leetcode.com/problems/sort-array-by-parity/description/
+"""
+
+class Solution:
+  def sortArrayByParity(self, nums):
+    l = 0
+    r = len(nums) - 1
+    while l < r:
+      if nums[l] % 2 == 1 and nums[r] % 2 == 0:
+        nums[l], nums[r] = nums[r], nums[l]
+      if nums[l] % 2 == 0:
+        l += 1
+      if nums[r] % 2 == 1:
+        r -= 1
+    return nums
