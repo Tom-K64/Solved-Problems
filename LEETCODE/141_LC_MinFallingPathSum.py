7524810b73d344880e21cc8b435643d8954a6027
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-01-31 16:34:38 +0530

    Q-931

---
+++ LEETCODE/141_LC_MinFallingPathSum.py
@@ -0,0 +1,15 @@
+"""
+Problem Link:
+https://leetcode.com/problems/minimum-falling-path-sum/description/
+"""
+
+class Solution:
+  def minFallingPathSum(self, A):
+    n = len(A)
+    for i in range(1, n):
+      for j in range(n):
+        mini = math.inf
+        for k in range(max(0, j - 1), min(n, j + 2)):
+          mini = min(mini, A[i - 1][k])
+        A[i][j] += mini
+    return min(A[-1])
