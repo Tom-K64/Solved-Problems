f231428e6e5ae41623970fc4d2337e7222ce30a0
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-02-12 23:50:30 +0530

    Q-1050

---
+++ LEETCODE/156_LC_ActorsDirector.py
@@ -0,0 +1,8 @@
+
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    
+    result = actor_director.groupby(['actor_id', 'director_id']).count().reset_index()
+    
+    return result[result['timestamp']>=3][['actor_id', 'director_id']]
