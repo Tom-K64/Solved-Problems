f8631a6c5348075cc149b32f74c0a844aed33f3f
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-02-17 12:20:16 +0530

    Q-1160

---
+++ LEETCODE/166_LC_FindWordsThat.py
@@ -0,0 +1,27 @@
+"""
+Problem Link:
+https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+"""
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        counts = [0] * 26
+        for c in chars:
+            counts[ord(c) - ord("a")] += 1
+        
+        ans = 0
+        for word in words:
+            word_count = [0] * 26
+            for c in word:
+                word_count[ord(c) - ord("a")] += 1
+            
+            good = True
+            for i in range(26):
+                if counts[i] < word_count[i]:
+                    good = False
+                    break
+            
+            if good:
+                ans += len(word)
+            
+        return ans
