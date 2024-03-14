209f86a9ecb5c3cb569fb2642f6cd43e4f5231f8
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-03-14 19:11:41 +0530

    Q-1247

---
+++ LEETCODE/191_LC_MinNumStep.py
@@ -0,0 +1,10 @@
+"""
+Problem Link:
+https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+"""
+
+class Solution:
+  def minSteps(self, s, t):
+    count = collections.Counter(s)
+    count.subtract(collections.Counter(t))
+    return sum(abs(value) for value in count.values()) // 2
