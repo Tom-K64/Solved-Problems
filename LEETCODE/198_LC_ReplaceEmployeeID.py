79169fea91e2b45e2df2e44ca4874b9a7bdf157b
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-03-18 11:38:39 +0530

    Q-1378

---
+++ LEETCODE/198_LC_ReplaceEmployeeID.py
@@ -0,0 +1,12 @@
+"""
+Problem Link:
+https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/
+"""
+
+import pandas as pd
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    
+    result = pd.merge(employees, employee_uni, on='id', how='left')
+    
+    return result[['unique_id', 'name']]
