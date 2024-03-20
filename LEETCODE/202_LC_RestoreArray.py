87ada547e3354b3e4ef3c6b4caf4673beaa54e90
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-03-20 12:36:41 +0530

    Q-1416

---
+++ LEETCODE/202_LC_RestoreArray.py
@@ -0,0 +1,20 @@
+"""
+Problem Link:
+https://leetcode.com/problems/restore-the-array/
+"""
+
+class Solution:
+    def numberOfArrays(self, s, k):
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[-1] = 1
+        for i in range(n - 1, -1, -1):
+            if s[i] == '0':
+                continue
+            num = 0
+            j = i
+            while j < n and int(s[i:j+1]) <= k:
+                num += dp[j+1]
+                j += 1
+            dp[i] = num % (10 ** 9 + 7)
+        return dp[0]
