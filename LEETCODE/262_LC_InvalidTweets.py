ea6bfe2f79c3a1b3acdd75dd3d75e04acc4fc5cd
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-05-15 16:39:52 +0530

    Q-1683

---
+++ LEETCODE/262_LC_InvalidTweets.py
@@ -0,0 +1,11 @@
+"""
+Problem Link:
+https://leetcode.com/problems/invalid-tweets/
+"""
+
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    return tweets[
+        tweets['content'].str.len() > 15
+    ][['tweet_id']]
