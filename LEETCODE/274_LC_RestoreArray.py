a92d8b1d369e1bb33648f190898db8eeb5023d4c
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-05-21 13:05:22 +0530

    Q-1743

---
+++ LEETCODE/274_LC_RestoreArray.py
@@ -0,0 +1,30 @@
+"""
+Problem Link:
+https://leetcode.com/problems/restore-the-array-from-adjacent-pairs/
+"""
+
+class Solution:
+  def restoreArray(self, adjacentPairs):
+    ans = []
+    numToAdjs = collections.defaultdict(list)
+
+    for a, b in adjacentPairs:
+      numToAdjs[a].append(b)
+      numToAdjs[b].append(a)
+
+    for num, adjs in numToAdjs.items():
+      if len(adjs) == 1:
+        ans.append(num)
+        ans.append(adjs[0])
+        break
+
+    while len(ans) < len(adjacentPairs) + 1:
+      tail = ans[-1]
+      prev = ans[-2]
+      adjs = numToAdjs[tail]
+      if adjs[0] == prev:
+        ans.append(adjs[1])
+      else:
+        ans.append(adjs[0])
+
+    return ans
