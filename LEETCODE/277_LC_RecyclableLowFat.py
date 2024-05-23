493c3f491dd0c9950f61eb4bff5d47ec42df9f54
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-05-23 22:12:53 +0530

    Q-1757

---
+++ LEETCODE/277_LC_RecyclableLowFat.py
@@ -0,0 +1,13 @@
+"""
+Problem Link:
+https://leetcode.com/problems/recyclable-and-low-fat-products/
+"""
+
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    df = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
+
+    df = df[['product_id']]
+    
+    return df
