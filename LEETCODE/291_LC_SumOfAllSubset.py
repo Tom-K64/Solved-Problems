521a9380cfcfa01c5b57c4f10f6172eda7582aac
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-06-06 22:54:18 +0530

    Q-1863

---
+++ LEETCODE/291_LC_SumOfAllSubset.py
@@ -0,0 +1,16 @@
+"""
+Problem Link:
+https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+"""
+
+class Solution:
+  def subsetXORSum(self, nums: List[int]) -> int:
+    def dfs(i: int, xors: int) -> int:
+      if i == len(nums):
+        return xors
+
+      x = dfs(i + 1, xors)
+      y = dfs(i + 1, nums[i] ^ xors)
+      return x + y
+
+    return dfs(0, 0)
