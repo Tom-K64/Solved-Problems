33a9fd2d24f37e1cab7133ec2f429cde6d43d754
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-07-05 19:32:40 +0530

    Q-2356

---
+++ LEETCODE/343_LC_NumberUniqueSubject.py
@@ -0,0 +1,12 @@
+"""
+Problem Link:
+https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/
+"""
+
+import pandas as pd
+
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    result = teacher.groupby('teacher_id')['subject_id'].nunique().reset_index()
+    result.rename(columns={'subject_id': 'cnt'}, inplace=True)
+    
+    return result
