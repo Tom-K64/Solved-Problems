c4f1f9a7d6b3fcbf07ba75839045c3cec038618b
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-08-09 19:48:15 +0530

    Q-2418

---
+++ LEETCODE/355_LC_SortThePeople.py
@@ -0,0 +1,9 @@
+"""
+Problem Link:
+https://leetcode.com/problems/sort-the-people/
+"""
+
+class Solution:
+  def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+    return [height for _, height in
+            sorted([(height, name) for name, height in zip(names, heights)], reverse=True)]
