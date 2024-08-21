a7d04eed92251baf3d20516f4e94ad65fff140e5
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-08-21 23:56:01 +0530

    Q-2486

---
+++ LEETCODE/366_LC_AppendCharStr.py
@@ -0,0 +1,16 @@
+"""
+Problem Link:
+https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/
+"""
+
+class Solution:
+  def appendCharacters(self, s: str, t: str) -> int:
+    i = 0  # t's index
+
+    for c in s:
+      if c == t[i]:
+        i += 1
+        if i == len(t):
+          return 0
+
+    return len(t) - i
