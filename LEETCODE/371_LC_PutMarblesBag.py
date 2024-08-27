31e83c3e731cf9741bffa0b32fedd23e6856850c
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-08-27 22:29:50 +0530

    Q-2551

---
+++ LEETCODE/371_LC_PutMarblesBag.py
@@ -0,0 +1,17 @@
+"""
+Problem Link:
+https://leetcode.com/problems/put-marbles-in-bags/
+"""
+
+class Solution:
+    def putMarbles(self, weights, k):
+        n = len(weights)
+        pair_weights = [0] * (n - 1)
+        for i in range(n - 1):
+            pair_weights[i] = weights[i] + weights[i + 1]
+        pair_weights.sort()
+        answer = 0
+        for i in range(k - 1):
+            answer += pair_weights[n - 2 - i] - pair_weights[i]
+            
+        return answer
