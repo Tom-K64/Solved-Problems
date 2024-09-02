dbbccf0b111bdbca860c3550059a78f4655d4bed
Kanhaya Kataruka <97863897+Tom-K64@users.noreply.github.com>
2024-09-02 11:22:29 +0530

    Q-2582

---
+++ LEETCODE/373_LC_PassThePillow.py
@@ -0,0 +1,12 @@
+"""
+Problem Link:
+https://leetcode.com/problems/pass-the-pillow/
+"""
+
+class Solution:
+  def passThePillow(self, n: int, time: int) -> int:
+    # Repeat every (n - 1) * 2 seconds.
+    time %= (n - 1) * 2
+    if time < n:  # Go forward from 1.
+      return 1 + time
+    return n - (time - (n - 1))  # Go backward from n.
